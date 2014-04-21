35c39b0d45b00e38594fd00b7c64ec47ecaf8c21
Mathieu Helie <mhelie@gmail.com>
2014-04-21 11:08:54 -0400

    Moved data to separate file

---
+++ playlist-data.js
@@ -0,0 +1,47 @@
+var playlistData = [
+  {
+    "path": "PEA",
+    "section": "Produits financiers",
+    "title" : "01-PEA- Protection Extra Attentive v07",
+    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "sources" : {
+      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/01-PEA-ProtectionExtraAttentivev07SORTIE-res360p-rate550k.mp4"
+    }
+  }
+  ,{
+    "path": "Assurance-de-Remplacement",
+    "section": "Produits financiers",
+    "title" : "02- Assurance de remplacement v05",
+    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "sources" : {
+      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/02-Assurancederemplacementv05SORTIE-res360p-rate550k.mp4"
+    }
+  }
+  ,{
+    "path": "Carte-d-Usure",
+    "section": "Produits financiers",
+    "title" : "03- Carte Usure -v04",
+    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "sources" : {
+      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/03-CarteUsure-v04-SORTIE-res360p-rate550k.mp4"
+    }
+  }
+  ,{
+    "path" : "Cueillette",
+    "section": "Livraison",
+    "title" : "04- Cueillette du vehicule v06",
+    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "sources" : {
+      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/04-Cueilletteduvehiculev06SORTIE-res360p-rate550k.mp4"
+    }
+  }
+  ,{
+    "path" : "Retour-a-la-maison",
+    "section": "Livraison",
+    "title" : "05- Retour a la maison v03",
+    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "sources" : {
+      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/05-Retouralamaisonv03-SORTIE-res360p-rate550k.mp4"
+    }
+  }
+];
