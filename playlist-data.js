6186bb47495363f67370957271810dc05c7de61b
Agence Webdiffusion <mathieu@agencewebdiffusion.com>
2014-04-22 13:56:39 -0400

    Update playlist data

--- playlist-data.js
+++ playlist-data.js
@@ -2,46 +2,82 @@ var playlistData = [
   {
     "path": "PEA",
     "section": "Produits financiers",
-    "title" : "01-PEA- Protection Extra Attentive v07",
-    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "title" : "PEA - Protection Extra Attentive",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/1-PEA_platine.jpg",
     "sources" : {
-      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/01-PEA-ProtectionExtraAttentivev07SORTIE-res360p-rate550k.mp4"
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/01-PEA-ProtectionExtraAttentivev07SORTIE.mp4"
     }
   }
   ,{
     "path": "Assurance-de-Remplacement",
     "section": "Produits financiers",
-    "title" : "02- Assurance de remplacement v05",
-    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "title" : "Assurance de remplacement",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/2_remplacement.jpg",
     "sources" : {
-      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/02-Assurancederemplacementv05SORTIE-res360p-rate550k.mp4"
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/02-Assurancederemplacementv-05-1.mp4"
     }
   }
   ,{
     "path": "Carte-d-Usure",
     "section": "Produits financiers",
-    "title" : "03- Carte Usure -v04",
-    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "title" : "Carte Usure",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/3_CarteUsure.jpg",
     "sources" : {
-      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/03-CarteUsure-v04-SORTIE-res360p-rate550k.mp4"
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/03-CarteUsure-v04-SORTIE.mp4"
     }
   }
   ,{
+    "path": "Antirouille",
+    "section": "Produits financiers",
+    "title" : "Antirouille",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/6-antirouille.jpg",
+    "sources" : {
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/06-Antirouillev-06-SORTIE.mp4"
+    }
+  }
+    ,{
+    "path": "Protection-des-tissus",
+    "section": "Produits financiers",
+    "title" : "Protection des tissus",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/7-tissu2.jpg",
+    "sources" : {
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/07-Protectiondestissusv-02-SORTIE2.mp4"
+    }
+  }
+    ,{
+    "path": "Sherlock",
+    "section": "Produits financiers",
+    "title" : "Sherlock",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/8-sherlock.jpg",
+    "sources" : {
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/08-Sherlockv-03-SORTIE.mp4"
+    }
+  }
+    ,{
+    "path": "TAG",
+    "section": "Produits financiers",
+    "title" : "TAG",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/9-tag.jpg",
+    "sources" : {
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/09-TAGv-04-SORTIE.mp4"
+    }
+  }
+    ,{
     "path" : "Cueillette",
     "section": "Livraison",
-    "title" : "04- Cueillette du vehicule v06",
-    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "title" : "Cueillette du vehicule",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/4_Ceuillette.jpg",
     "sources" : {
-      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/04-Cueilletteduvehiculev06SORTIE-res360p-rate550k.mp4"
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/04-Cueilletteduvehiculev06SORTIE.mp4"
     }
   }
   ,{
     "path" : "Retour-a-la-maison",
     "section": "Livraison",
-    "title" : "05- Retour a la maison v03",
-    "poster" : "http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg",
+    "title" : "Retour a la maison",
+    "poster" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/posters/5-retour.jpg",
     "sources" : {
-      "video/mp4" : "http://vimonttoyota.agencewebdiffusion.com.s3.amazonaws.com/videos/05-Retouralamaisonv03-SORTIE-res360p-rate550k.mp4"
+      "video/mp4" : "http://d28a99uh4on6s8.cloudfront.net/videos/HTML5V2/05-Retouralamaisonv03-SORTIE.mp4"
     }
-  }
+  }  
 ];
